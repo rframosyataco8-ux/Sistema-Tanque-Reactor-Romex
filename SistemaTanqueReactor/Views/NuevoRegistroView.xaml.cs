154a5fac1c71db11362e57b2cd881b8b02831844
Fabricio_Ramos <rframosyataco8@gmail.com>
2026-09-21 13:11:55 -0500

    Enter en cantidad ejecuta el cálculo

--- SistemaTanqueReactor/Views/NuevoRegistroView.xaml.cs
+++ SistemaTanqueReactor/Views/NuevoRegistroView.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows.Controls;
+using System.Windows.Input;
+using SistemaTanqueReactor.ViewModels;
 
 namespace SistemaTanqueReactor.Views;
 
@@ -7,5 +9,18 @@ public partial class NuevoRegistroView : UserControl
     public NuevoRegistroView()
     {
         InitializeComponent();
+    }
+
+    private void TxtCantidad_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter || e.Key == Key.Return)
+        {
+            e.Handled = true;
+            if (DataContext is NuevoRegistroViewModel vm)
+            {
+                if (vm.CalcularCantidadCommand.CanExecute(null))
+                    vm.CalcularCantidadCommand.Execute(null);
+            }
+        }
     }
 }
