709088173dbc091ae2c6fb28c59b9a53dfe2d90e
Fabricio_Ramos <rframosyataco8@gmail.com>
2026-09-22 15:16:34 -0500

    Fix Dashboard code-behind sin referencias rotas a Glow

--- SistemaTanqueReactor/Views/DashboardView.xaml.cs
+++ SistemaTanqueReactor/Views/DashboardView.xaml.cs
@@ -1,6 +1,4 @@
-using System.Windows;
 using System.Windows.Controls;
-using SistemaTanqueReactor.Helpers;
 
 namespace SistemaTanqueReactor.Views;
 
@@ -9,16 +7,5 @@ public partial class DashboardView : UserControl
     public DashboardView()
     {
         InitializeComponent();
-        Loaded += OnLoaded;
-    }
-
-    private void OnLoaded(object sender, RoutedEventArgs e)
-    {
-        NeonAnimations.FadeIn(this, 0.4);
-        NeonAnimations.Pulse(Glow1, delay: 0.0);
-        NeonAnimations.Pulse(Glow2, delay: 0.25);
-        NeonAnimations.Pulse(Glow3, delay: 0.5);
-        NeonAnimations.Pulse(Glow4, delay: 0.75);
-        NeonAnimations.Pulse(StatusDot, delay: 0.1);
     }
 }
